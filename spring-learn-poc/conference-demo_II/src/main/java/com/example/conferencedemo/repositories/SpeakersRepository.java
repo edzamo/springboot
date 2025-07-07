9b278ffd9bd7cba0b0565ae373ae718ed93be9df
edzamo <edzamo13@gmail.com>
2025-07-07 10:44:16 -0500

    add project with chatgtp

--- spring-learn-poc/conference-demo_II/src/main/java/com/example/conferencedemo/repositories/SpeakersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.conferencedemo.repositories;
-
-import com.example.conferencedemo.models.Speakers;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpeakersRepository extends ReactiveCrudRepository<Speakers, Long> {
-}
